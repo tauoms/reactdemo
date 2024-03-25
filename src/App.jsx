d11688fd5cffe68d3eafbe705c7309a970f2f33e
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-25 14:08:05 +0200

    Add key to listed box components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import Box from './Box'
 import { useState } from 'react'
 
 function App() {
-const [counter, setCounter] = useState('string');
+  const [counter, setCounter] = useState('string');
+  const [persons, setPersons] = useState([
+    {id:1, name:"Jeeves", title:"CEO", location:"Tampere"},
+    {id:2, name:"Hannah", title:"CTO", location:"Joensuu"},
+    {id:3, name:"Jake", title:"UFO", location:"Rauma"},
+  ]);
 
-return (
-    <>
-    <p>Counter: {counter}</p>
-      <Box name="Jeeves" title="CEO" location="Tampere" />
-      <Box name="Hannah" title="CTO" location="Joensuu" />
-      <Box name="Jake" title="UFO" location="Rauma" />
-    </>
-  )
+  return (
+      <>
+        <p>Counter: {counter}</p>
+        {persons.map(person => (
+        <Box 
+          key={person.id}
+          name={person.name} 
+          title={person.title} 
+          location={person.location} 
+        />
+        ))}
+      </>
+    );
 }
 
 export default App
