d60b85ab07d625ef831061d4763e7ffa9ebb65fb
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-25 13:33:34 +0200

    Add import useState and use it for counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Box from './Box'
+import { useState } from 'react'
 
 function App() {
+const [counter, setCounter] = useState('string');
 
-  return (
+return (
     <>
+    <p>Counter: {counter}</p>
       <Box name="Jeeves" title="CEO" location="Tampere" />
       <Box name="Hannah" title="CTO" location="Joensuu" />
       <Box name="Jake" title="UFO" location="Rauma" />
