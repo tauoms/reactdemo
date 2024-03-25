5b59270d025de9ec63f0d3fb81c2aa59b36c8c14
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-25 14:55:00 +0200

    Add buttons to increase, decrease and reset counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import './App.css'
-import Box from './Box'
-import { useState } from 'react'
+import "./App.css";
+import Box from "./Box";
+import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState('string');
+  const [counter, setCounter] = useState(0);
   const [persons, setPersons] = useState([
-    {id:1, name:"Jeeves", title:"CEO", location:"Tampere"},
-    {id:2, name:"Hannah", title:"CTO", location:"Joensuu"},
-    {id:3, name:"Jake", title:"UFO", location:"Rauma"},
+    { id: 1, name: "Jeeves", title: "CEO", location: "Tampere" },
+    { id: 2, name: "Hannah", title: "CTO", location: "Joensuu" },
+    { id: 3, name: "Jake", title: "UFO", location: "Rauma" },
   ]);
 
+  function clickIncrease() {
+    setCounter(counter + 1);
+  }
+  function clickDecrease() {
+    setCounter(counter - 1);
+  }
+  function clickResetCounter() {
+    setCounter(0);
+  }
+
   return (
-      <>
-        <p>Counter: {counter}</p>
-        {persons.map(person => (
-        <Box 
+    <>
+      <p>Counter: {counter}</p>
+      <button onClick={clickIncrease}>Increase +1</button>
+      <button onClick={clickDecrease}>Decrease -1</button>
+      <button onClick={clickResetCounter}>Reset</button>
+      {persons.map((person) => (
+        <Box
           key={person.id}
-          name={person.name} 
-          title={person.title} 
-          location={person.location} 
+          name={person.name}
+          title={person.title}
+          location={person.location}
         />
-        ))}
-      </>
-    );
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;
