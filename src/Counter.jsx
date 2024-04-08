fdbf433362d85efc5b1c2bb88ccf5cad71323713
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-08 13:22:56 +0300

    Add Counter styling

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,7 @@
 const Counter = (props) => {
   return (
-    <div>
-      <p>Counter: {props.counterProp}</p>
+    <div className="counterDiv">
+      <p className="counterP">Counter: {props.counterProp}</p>
       <button onClick={props.clickIncrease}>Increase +1</button>
       <button onClick={props.clickDecrease}>Decrease -1</button>
       <button onClick={props.clickResetCounter}>Reset</button>
