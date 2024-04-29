85b349c8d04b2a54a9f7ff237ae0f735633cfd07
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-29 12:18:08 +0300

    Create Root component

---
+++ src/Root.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h2>Content will be changing</h2>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Root;
