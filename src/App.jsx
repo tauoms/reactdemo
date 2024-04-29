bc1b4583f9a67b8a117b579a7f590f3a1952d90e
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-29 10:12:05 +0300

    Create Dashboard component, add conditional rendering for it

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import "./App.css";
 import Box from "./Box";
 import Header from "./Header";
 import Footer from "./Footer";
-import Counter from "./Counter";
 import Form from "./Form";
 import View from "./View";
 import { useState } from "react";
+import Dashboard from "./Dashboard";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [persons, setPersons] = useState([
-    { id: 1, name: "Jeeves", title: "CEO", location: "Tampere" },
-    { id: 2, name: "Hannah", title: "CTO", location: "Joensuu" },
-    { id: 3, name: "Jake", title: "UFO", location: "Rauma" },
-  ]);
+  // const [counter, setCounter] = useState(0);
+  // const [persons, setPersons] = useState([
+  //   { id: 1, name: "Jeeves", title: "CEO", location: "Tampere" },
+  //   { id: 2, name: "Hannah", title: "CTO", location: "Joensuu" },
+  //   { id: 3, name: "Jake", title: "UFO", location: "Rauma" },
+  // ]);
   const [inputValue, setInputValue] = useState("");
   const [formData, setFormData] = useState({
     fname: "",
@@ -22,6 +22,7 @@ function App() {
     tel: "",
     message: "",
   });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const changeHandler = (event) => {
     setInputValue(event.target.value);
@@ -30,7 +31,7 @@ function App() {
   const changeFormHandler = (event) => {
     const { name, value } = event.target; // Destructured
 
-    // open previous state, check if object has fname etc. and replace value
+    // open previous state, check if object has fname etc. and replace value with old + new data
     setFormData((prevState) => ({ ...prevState, [name]: value }));
     console.log(formData);
   };
@@ -48,25 +49,12 @@ function App() {
   return (
     <>
       <Header />
-      <Form changeFormHandler={changeFormHandler} />
-      <View {...formData} />
+      {isLoggedIn && <Dashboard user="Someone" />}
 
-      {/* <Counter
-        counterProp={counter}
-        clickIncrease={clickIncrease}
-        clickDecrease={clickDecrease}
-        clickResetCounter={clickResetCounter}
-      />
-      <div className="boxContainer">
-        {persons.map((person) => (
-          <Box
-            key={person.id}
-            name={person.name}
-            title={person.title}
-            location={person.location}
-          />
-        ))}
-      </div> */}
+      {/* <Form changeFormHandler={changeFormHandler} /> */}
+      {/* Pass spread formData as prop */}
+      {/* <View {...formData} /> */}
+
       <Footer />
     </>
   );
---
+++ src/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = ({ user }) => {
+  return (
+    <div>
+      <h2>Welcome to inner circle, {user}</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
